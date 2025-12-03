3afac0335140cc20e0e73ecd1742b92864084afc
Kunal Bhalla <bhalla.kunal@gmail.com>
2025-12-03 10:55:02 +0530

    Clean up a little bit

--- day03/day03.py
+++ day03/day03.py
@@ -1,40 +1,30 @@
 import sys
 
-def main2():
-    sum = 0
+
+def main():
+    sum1, sum2 = 0, 0
     for joltage in sys.stdin:
         nos = list(map(int, list(joltage.strip())))
+
+        a = max(nos[:-1])
+        ai = nos.index(a)
+        b = max(nos[ai + 1:])
+        sum1 += 10 * a + b
 
         start = 0
         stop = -11
         no = 0
-        #print(joltage)
+        l = len(nos)
         for i in range(12):
-            if stop < 0:
-                rnos = nos[start:stop]
-            else:
-                rnos = nos[start:]
+            rnos = nos[start:(l + stop)]
             xi = max(rnos)
             no = no * 10 + xi
             start = start + rnos.index(xi) + 1
             stop += 1
-        # print(xi, start, stop, no)
-        sum += no
+        sum2 += no
 
-    print(sum)
-
-
-def main():
-    sum = 0
-    for joltage in sys.stdin:
-        nos = list(map(int, list(joltage.strip())))
-        a = max(nos[:-1])
-        ai = nos.index(a)
-        b = max(nos[ai + 1:])
-        sum += 10 * a + b
-        #  print(joltage, b, a)
-    print(sum)
+    print(sum1)
+    print(sum2)
 
 if __name__ == "__main__":
-    # main()
-    main2()
+    main()
