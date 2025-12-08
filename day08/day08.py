65f6eba8756acfcb48a103fc6643ff4cb4dcdfe4
Kunal Bhalla <bhalla.kunal@gmail.com>
2025-12-08 11:20:13 +0530

    Print both solutions

--- day08/day08.py
+++ day08/day08.py
@@ -10,6 +10,8 @@ def main():
             continue
         boxes.append(list(map(int, row.split(","))))
 
+    count = len(boxes)
+
     distances = []
     for i in range(0, len(boxes)):
         for j in range(i + 1, len(boxes)):
@@ -22,41 +24,40 @@ def main():
     distances.sort(key=lambda x: x[0])
 
     circuit_id = 1
-    circuits = [0] * 1000
+    circuits = [0] * count
     steps = 0
     for pair in distances:
-        # print(pair, boxes[pair[1]], boxes[pair[2]])
         circuit1 = circuits[pair[1]]
         circuit2 = circuits[pair[2]]
         if circuit1 == circuit2 and circuit1 != 0:
-            continue
-        if circuit1 == 0 and circuit2 == 0:
+            ...
+        elif circuit1 == 0 and circuit2 == 0:
             circuits[pair[1]] = circuit_id
             circuits[pair[2]] = circuit_id
             circuit_id += 1
-        if circuit1 == 0 and circuit2 != 0:
+        elif circuit1 == 0 and circuit2 != 0:
             circuits[pair[1]] = circuit2
-        if circuit1 != 0 and circuit2 == 0:
+        elif circuit1 != 0 and circuit2 == 0:
             circuits[pair[2]] = circuit1
-        if circuit1 != 0 and circuit2 != 0:
+        elif circuit1 != 0 and circuit2 != 0:
             total = 0
-            for i in range(1000):
+            for i in range(count):
                 if circuits[i] == circuit1 or circuits[i] == circuit2:
                     circuits[i] = circuit_id
                     total += 1
-            if total == 1000:
+            if total == count:
                 print(boxes[pair[1]][0] * boxes[pair[2]][0])
                 return
 
             circuit_id += 1
 
-    # print(circuits[:20])
-    sizes = defaultdict(lambda: 0)
-    for val in circuits:
-        if val != 0:
-            sizes[val] += 1
-    print(sizes)
-    print(math.prod(sorted(sizes.values(), reverse=True)[:3]))
+        steps += 1
+        if steps == 1000:
+            sizes = defaultdict(lambda: 0)
+            for val in circuits:
+                if val != 0:
+                    sizes[val] += 1
+            print(math.prod(sorted(sizes.values(), reverse=True)[:3]))
 
 
 if __name__ == "__main__":
